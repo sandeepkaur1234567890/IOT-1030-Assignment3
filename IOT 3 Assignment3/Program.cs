b5017154c528547853b6d5181ed1bc49e45008d9
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2021-10-18 22:44:14 -0400

    second commit
    I am adding program.cs file in Task 1.

--- IOT 3 Assignment3/Program.cs
+++ IOT 3 Assignment3/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+using IOT_3_Assignment3.Birds;
+using IOT_3_Assignment3.Eggs;
+
 namespace IOT_3_Assignment3
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Bird bird;
+				Console.Write("Press C for chicken or O for Ostrich: ");
+				char key = Char.ToUpper(Console.ReadKey().KeyChar);
+				switch (key)
+				{
+					case 'C':
+						bird = new Chicken();
+						break;
+					case 'O':
+						bird = new Ostrich();
+						break;
+					default:
+						return;
+				}
+				Console.Write("\nHow many eggs should it lay? ");
+				if (!int.TryParse(Console.ReadLine(), out int numEggs)) { return; }
+				Egg[] eggs = bird.LayEggs(numEggs);
+				DisplayEggs(eggs);
+			}
+		}
+
+		static void DisplayEggs(Egg[] eggs)
+		{
+			foreach (var egg in eggs)
+			{
+				Console.WriteLine(egg);
+			}
+		}
+	}
 }
