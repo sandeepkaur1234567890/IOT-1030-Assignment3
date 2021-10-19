d193cc3f7ee90685512a54ee76eaa206b7bedbdb
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2021-10-18 22:23:31 -0400

    first commit
    I can complete Task 1

---
+++ IOT 3 Assignment3/Birds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using IOT_3_Assignment3.Eggs;
+namespace IOT_3_Assignment3.Birds
+{
+	class Bird
+	{
+		public static Random Rand = new Random(1);
+		public virtual Egg[] LayEggs(int numEggs)
+		{
+			Console.Error.WriteLine("Bird.LayEggs should not be called!");
+			return new Egg[0];
+		}
+	}
+
+	class Chicken : Bird
+	{
+		// TODO: Implement this class
+		public override Egg[] LayEggs(int numEggs)
+		{
+			Egg[] chicken = new Egg[numEggs];
+			for (int c = 0; c < numEggs; c++)
+			{
+				chicken[c] = new Egg(Rand.Next(2,4), Egg.Colors.brown);
+			}
+			return chicken;
+		}
+	}
+
+	class Ostrich : Bird
+	{
+		// TODO: Implement this class
+		public override Egg[] LayEggs(int numEggs)
+		{
+			Egg[] ostrich = new Egg[numEggs];
+			for (int o = 0; o < numEggs; o++)
+			{
+				ostrich[o] = new Egg(Rand.Next(10, 15), Egg.Colors.speckled);			
+			}
+			return ostrich;
+		}
+
+	}
+}
---
+++ IOT 3 Assignment3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOT_3_Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
